02f4945c533c93843aa08a52cb102ae8c4127ae4
KangJinha <121327609+KangJinha@users.noreply.github.com>
2023-06-09 18:57:04 +0900

    속성별분류

--- js/main.js
+++ js/main.js
@@ -1,42 +1,80 @@
-const listaPokemon = document.querySelector("#listaPokemon");
+const readyPokemon = document.querySelector("#readyPokemon");
+const buttonsHeader = document.querySelectorAll(".btn-header");
 
-let URL = "https://pokeapi.co/api/v2/pokemon-species/";
-
-for (let i = 1; i <= 151; i++) {
+let URL = "https://pokeapi.co/api/v2/pokemon/";
+//let URL = "https://pokeapi.co/api/v2/pokemon-species/";
+//한국이름은 이걸로 가져와야하는데...
+for (let i = 1; i <= 160; i++) {
     fetch(URL + i)
         .then((res) => res.json())
-        .then((data) => mostrarPokemon(data));
+        .then((data) => showPokemon(data));
 }
-//fetch로 가져온데이타를 mostrarPokemon함수로 가져온다
-function mostrarPokemon(poke) {
+//fetch로 가져온데이타를 showPokemon함수로 가져온다
+
+function showPokemon(poke) {
+    let type = poke.types.map(
+        (type) => `<p class="${type.type.name} type">${type.type.name}</p>`
+    );
+    type = type.join("");
+    //array형식으로 되어있는것을 string로 변경!!
+
+    let pokeId = poke.id.toString();
+    if (pokeId.length === 1) {
+        pokeId = "00" + pokeId;
+    } else if (pokeId.length === 2) {
+        pokeId = "0" + pokeId;
+    }
+    //pokeId 자리수맞춰주기
+
     const div = document.createElement("div");
     div.classList.add("pokemon");
     div.innerHTML = `
-        <p class="pokemon-id-back">#025</p>
+        <p class="pokemon-id-back">#${pokeId}</p>
         <div class="class pokemon-imagen">
             <img
-                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"
-                alt="Pikachu"
+                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${poke.id}.png"
+                alt="${poke.id}"
             />
         </div>
         <div class="pokemon-info">
             <div class="nombre-contenedor">
-                <p class="pokemon-id">#025</p>
-                <h2 class="pokemon-nombre">${poke.names[2].name}</h2>
+                <p class="pokemon-id">#${pokeId}</p>
+                <h2 class="pokemon-nombre">${poke.name}</h2>
             </div>
-            <div class="pokemon-tipos">
-                <p class="electric tipo">ELECTRIC</p>
-                <p class="fighting tipo">FIGHTING</p>
+            <div class="pokemon-types">
+                ${type}
             </div>
             <div class="pokemon-stats">
-                <p class="stat">4M</p>
-                <p class="stat">60KG</p>
+                <p class="stat">${poke.height}M</p>
+                <p class="stat">${poke.weight}KG</p>
             </div>
         </div>
     `;
-    listaPokemon.append(div);
+    readyPokemon.append(div);
 }
 
+buttonsHeader.forEach((btn) =>
+    btn.addEventListener("click", (event) => {
+        const btnId = event.currentTarget.id;
+
+        readyPokemon.innerHTML = "";
+
+        for (let i = 1; i <= 160; i++) {
+            fetch(URL + i)
+                .then((res) => res.json())
+                .then((data) => {
+                    if (btnId === "all") {
+                        showPokemon(data);
+                    } else {
+                        const type = data.types.map((type) => type.type.name);
+                        if (type.same((type) => type.includes(btnId))) {
+                            showPokemon(data);
+                        }
+                    }
+                });
+        }
+    })
+);
 /* <div class="pokemon">
     <p class="pokemon-id-back">#025</p>
     <div class="class pokemon-imagen">
@@ -50,9 +88,9 @@ function mostrarPokemon(poke) {
             <p class="pokemon-id">#025</p>
             <h2 class="pokemon-nombre">피카츄</h2>
         </div>
-        <div class="pokemon-tipos">
-            <p class="electric tipo">ELECTRIC</p>
-            <p class="fighting tipo">FIGHTING</p>
+        <div class="pokemon-types">
+            <p class="electric type">ELECTRIC</p>
+            <p class="fighting type">FIGHTING</p>
         </div>
         <div class="pokemon-stats">
             <p class="stat">4M</p>
